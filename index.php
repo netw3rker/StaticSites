5ef5c2db9789a85acdc3fafe760c2ed939bb92c4
Chris Porter <cbporter@gmail.com>
2023-06-01 10:25:48 -0400

    Added a Routing file that serves uploaded markup from the /files path

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+$url=parse_url($_SERVER['REQUEST_URI']);
+
+if(substr($url['path'], -1) === '/') {
+  $url['path'] .= 'index.html';
+}
+
+$ext = pathinfo($url['path'], PATHINFO_EXTENSION);
+
+
+if(file_exists("/files" . $url['path'])){
+
+  $content_type = mime_content_type("/files" . $url['path']);
+
+  header("Content-Type: " . $content_type);
+  // this will execute PHP that is embedded. This should be an option for users via a configuration file.
+  include("/files" . $url['path']);
+
+}
+else{
+  include("404.html");
+}
